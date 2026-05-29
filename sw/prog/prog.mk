3d597fa1dba4e2415b2c8b83354576fb773558e2
swarneel123bh-ux <swarneel123bh@gmail.com>
2026-05-29 16:46:40 +0530

    Refactored prog directory

--- sw/prog/prog.mk
+++ sw/prog/prog.mk
@@ -2,8 +2,9 @@
 ROOT := ../..
 include $(ROOT)/common.mk
 
+PROG_SRC	 := ./src
 PROG_CFG   := prog.cfg
-PROG_CRT0  := prog_crt0.s
+PROG_CRT0  := $(PROG_SRC)/prog_crt0.s
 PROG_OBJS  := $(BUILD_DIR)/prog_crt0.o \
               $(BUILD_DIR)/prog_hello.o \
               $(BUILD_DIR)/prog_syslib.o
@@ -13,11 +14,11 @@ PROG_OBJS  := $(BUILD_DIR)/prog_crt0.o \
 all: $(PROG_BBX)
 syslib: $(BUILD_DIR)/prog_syslib.o
 
-$(BUILD_DIR)/prog_%.s: %.c | $(BUILD_DIR)
+$(BUILD_DIR)/prog_%.s: $(PROG_SRC)/%.c | $(BUILD_DIR)
 	$(CC65) $(CC65_FLAGS) -o $@ $<
 $(BUILD_DIR)/prog_%.o: $(BUILD_DIR)/prog_%.s | $(BUILD_DIR)
 	$(CA65) $(CA65_FLAGS) -o $@ $<
-$(BUILD_DIR)/prog_syslib.o: syslib.s | $(BUILD_DIR)
+$(BUILD_DIR)/prog_syslib.o: $(PROG_SRC)/syslib.s | $(BUILD_DIR)
 	$(CA65) $(CA65_FLAGS) -o $@ $<
 $(BUILD_DIR)/prog_crt0.o: $(PROG_CRT0) | $(BUILD_DIR)
 	$(CA65) $(CA65_FLAGS) -o $@ $<
