774ebca7e093f07247ee788e399a983c357d56f4
swarneel123bh-ux <swarneel123bh@gmail.com>
2026-06-07 12:33:01 +0530

    Need to save memory so kernel fits in 1 physcal page (4kB). Also need to change mmu6502.v to disable translation to make brk and yield easier (and thus syscalls easier)

--- sw/kernel/src/main.c
+++ sw/kernel/src/main.c
@@ -15,38 +15,15 @@ int main(void) {
 	*(uint8_t*)0xFE = (uint8_t)((uint16_t)brk_handler);				// LOW BYTE
 	*(uint8_t*)0xFF = (uint8_t)((uint16_t)brk_handler >> 8);	// HIGH BYTE
 
-  //k_print("Kernel Booting...\n");
   // Identity map pages for kernel
-  *MMU_PPN0 = 0x00;
-  *MMU_PPN1 = 0x01;
-  *MMU_PPN2 = 0x02;
-  *MMU_PPN3 = 0x03;
-  *MMU_PPN4 = 0x04;
-  *MMU_PPN5 = 0x05;
-  *MMU_PPN6 = 0x06;
-  *MMU_PPN7 = 0x07;
-  // k_print("MMU Initialized. System Ready.\n");
-  // k_print("BB6502 Block Driver Test\n");
-  // k_print("Reading Boot Sector (LBA 0)\n");
+  for (i = 0; i < 8; ++i) {
+  	*(MMU_PPN0 + i) = i;
+  }
+
   if (block_read(0, block_buf)) {
   	k_print("block_read ERROR");
    	while (1);	// Hang on Boot Sector Read fail
   }
-
-  // Print first 16 bytes as hex
-  // k_print("First 16 bytes: ");
-  // for (i = 0; i < 16; i++) {
-  //   k_puthex(block_buf[i]);
-  //   k_print(" ");
-  // }
-  // k_print("\n");
-
-  // FAT16 boot sectors have $55 $AA at offset 510-511
-  // k_print("Sig bytes (510,511): ");
-  // k_puthex(block_buf[510]);
-  // k_print(" ");
-  // k_puthex(block_buf[511]);
-  // k_print("\n");
 
   if (block_buf[510] == 0x55 && block_buf[511] == 0xAA) {
   	k_print("FAT boot signature OK\r\n");
@@ -54,31 +31,8 @@ int main(void) {
   	k_print("FAT boot signature MISSING\r\n");
   }
 
-  k_print("hello.c\r\n");
   spawn_bbx(200, 1, 1);    // hello
-  k_print("PROCESS\r\n");
-  k_print("\r\n"); 						k_puthex(plist[0].state);
-  k_print("\r\n"); 							k_puthex(plist[0].lba);
-  k_print("\r\n"); 					k_puthex(plist[0].nblocks);
-  k_print("\r\n"); 								k_puthex(plist[0].SP);
-  k_print("\r\n");
-  for (i = 0; i < 8; ++i) {
- 		k_puthex(plist[0].mmu_ppn_table[i]);
-   	k_print("\r\n");
-  }
-  k_print("hello2.c\r\n");
   spawn_bbx(210, 1, 3);    // hello2
-  k_print("PROCESS\r\n");
-  k_print("\r\n"); 						k_puthex(plist[1].state);
-  k_print("\r\n"); 							k_puthex(plist[1].lba);
-  k_print("\r\n"); 					k_puthex(plist[1].nblocks);
-  k_print("\r\n"); 								k_puthex(plist[1].SP);
-  k_print("\r\n");
-  for (i = 0; i < 8; ++i) {
- 		k_puthex(plist[1].mmu_ppn_table[i]);
-   	k_print("\r\n");
-  }
-
   k_print("scheduler...\r\n");
   sched_start();           // never returns
 
